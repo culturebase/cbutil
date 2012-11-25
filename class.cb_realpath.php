c377574c21d2bba12d2e981340240d9beddf4a78
Ulf Hermann <ulfhermann@culturebase.org>
2012-11-25 15:32:31 +0000

    document CbRealpath

--- class.cb_realpath.php
+++ class.cb_realpath.php
@@ -5,16 +5,40 @@
  */
 class CbRealpath {
 
-   static function getFromModules(array $files, $prefix = '') {
-      return self::get($files, dirname(__FILE__)."/../../".$prefix.'/');
+   /**
+    * @deprecated Use getFromHere.
+    */
+   static function getFromModules($files, $prefix = '')
+   {
+      return self::getFromHere($files, $prefix);
    }
 
-   static function get(array $files, $prefix = '') {
+   /**
+    * Get the absolute paths of the given files which are located relative to
+    * this one. You can use that to find e.g. locations of JS or CSS files in
+    * projects included using PHP's inclusion mechanism without actually knowing
+    * the filesystem path of the included project. Unfortunately it doesn't
+    * scale so well, as you can not redeclare CbRealpath...
+    *
+    * @param array $files Files to be resolved.
+    * @param string $prefix Path prefix to be added when looking for them.
+    * @param int $levels_up Number of '../' to be added to escape from the
+    *                       subfolder this file is in.
+    * @return array Absolute paths to the given files.
+    */
+   static function getFromHere($files, $prefix = '', $levels_up = 2)
+   {
+      $parts = array_fill(0, $levels_up, '..');
+      $parts[] = $prefix;
+      return self::get($files, dirname(__FILE__).'/'.implode('/', $parts).'/');
+   }
+
+   static function get(array $files, $prefix = '')
+   {
       if ($prefix) $prefix .= '/'; // realpath strips double slashes
       $real = array();
-      foreach($files as $file) {
-         $real[] = realpath($prefix.$file);
-      }
+      foreach ($files as $file) $real[] = realpath($prefix . $file);
       return $real;
    }
+
 }
